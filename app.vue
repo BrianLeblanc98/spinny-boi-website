95dbefae35595f32c7ac14ad4b8c422a5970e87c
brleblanc98 <brleblanc98@gmail.com>
2023-07-12 15:25:46 -0400

    Add basic random choice component

--- app.vue
+++ app.vue
@@ -4,5 +4,8 @@
     <font-awesome-icon icon='fa-solid fa-user' class='fa-fw fa-lg'/>
     <font-awesome-icon icon='fa-brands fa-github' class='fa-fw fa-lg'/>
     <h1 class='text-3xl font-bold underline'>Spinny Boi</h1>
+    <SpinnyWheel
+    :options='["Lime Rock Park - Classic", "Mount Panorama", "Long Beach Street Circuit"]'
+    />
   </div>
 </template>
---
+++ components/SpinnyWheel.vue
@@ -0,0 +1,23 @@
+<script setup lang='ts'>
+const props = defineProps<{
+  options: string[]
+}>()
+import { ref } from 'vue';
+
+const track = ref('');
+
+function getRandomOption() {
+  const index = Math.floor(Math.random() * props.options.length);
+  track.value = props.options[index];
+}
+</script>
+
+<template>
+  <button
+    @click='getRandomOption'
+    class='bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded'
+  >
+    Get a random track
+  </button>
+  <h2>{{ track }}</h2>
+</template>
