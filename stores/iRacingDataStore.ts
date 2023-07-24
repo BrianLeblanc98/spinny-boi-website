a6d74c8a08076ff6dbe0c61c3a5aca0dcd56a7e6
brleblanc98 <brleblanc98@gmail.com>
2023-07-24 17:33:41 -0400

    Create getters in iRacingDataStore to replace createInitialOwnedData function

--- stores/iRacingDataStore.ts
+++ stores/iRacingDataStore.ts
@@ -2,5 +2,46 @@ export const useIRacingDataStore = defineStore('iRacingData', {
   state: () => ({
     data: {} as iRacingData,
   }),
-  getters: {},
+  getters: {
+    initialOwnedCarData: (state) => {
+      // Called when a new user signs in
+      // Create initial ownedPackages for cars to save to the user
+      const initialOwnedCarData = {} as ownedPackages
+
+      Object.keys(state.data.carData).forEach((packageId) => {
+        const carIds = Object.keys(state.data.carData[packageId].cars)
+
+        initialOwnedCarData[packageId] = {
+          name: state.data.carData[packageId].cars[carIds[0]].name,
+          owned: false,
+        }
+
+        if (state.data.carData[packageId].free) {
+          initialOwnedCarData[packageId].owned = true
+          initialOwnedCarData[packageId].free = true
+        }
+      })
+
+      return initialOwnedCarData
+    },
+    initialOwnedTrackData: (state) => {
+      // Called when a new user signs in
+      // Create initial ownedPackages for tracks to save to the user
+      const initialOwnedTrackData = {} as ownedPackages
+
+      Object.keys(state.data.trackData).forEach((packageId) => {
+        initialOwnedTrackData[packageId] = {
+          name: state.data.trackData[packageId].name,
+          owned: false,
+        }
+
+        if (state.data.trackData[packageId].free) {
+          initialOwnedTrackData[packageId].owned = true
+          initialOwnedTrackData[packageId].free = true
+        }
+      })
+
+      return initialOwnedTrackData
+    },
+  },
 })
